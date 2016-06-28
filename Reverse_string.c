e9640a3c240a3cc38cb1376276aac06bafad7157
GowthamSandeep G <gowthamsandeep16@gmail.com>
2016-06-28 13:38:12 +0530

    Create Reverse_string.c

---
+++ Reverse_string.c
@@ -0,0 +1,10 @@
+    #include<stdio.h>
+    #include<string.h>
+    void main()
+     {
+        char b[10000];
+         printf("\nEnter the string\n");
+        gets(b);
+        printf("\nReversed string is\n %s",strrev(b));
+    getch();
+    }
