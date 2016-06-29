a9e5bac7bd3a0a7ce8cab736f15913eaf70994e1
GowthamSandeep G <gowthamsandeep16@gmail.com>
2016-06-29 11:50:36 +0530

    Update greatest num of repeated letters in string.java

--- greatest num of repeated letters in string.java
+++ greatest num of repeated letters in string.java
@@ -6,19 +6,20 @@ public class check
     public static void main(String args[])
     public char getMax(String word)
     {
+        int i,x;
         int Count = 0;
         int max = 0;
         char maxCharacterChar = '.';
 
         char[] cArray = word.toCharArray();
 
-        for(int i =0; i < cArray.length; i++)
+        for(i =0; i < cArray.length; i++)
         {
             int characterASCII = (int)cArray[i];
             Count = 0;
 
             
-            for(int x = 0; x < cArray.length; x++)
+            for(x = 0; x < cArray.length; x++)
             {
                 if(characterASCII == (int)cArray[x])
                 {
