1e49b2549c9e964ec480a2abf4e17fb9c8fcbec4
GowthamSandeep G <gowthamsandeep16@gmail.com>
2016-07-01 15:20:57 +0530

    Update greatest of three numbers.c

--- greatest of three numbers.c
+++ greatest of three numbers.c
@@ -2,19 +2,19 @@
 int main()
 {
 int a,b,c;
-printf("enter the three numbers=");
-scanf("%d""%d""%d",&a,&b,&c);
-if(a>b&&a>c)
+printf("enter the 3 numbers=");
+scanf("%d""%d""%d",&p,&q,&r);
+if(p>q&&p>r)
 {
-printf("%d is greater\n",a);
+printf("%d is greater\n",p);
 }
-else if(b>a&&b>c)
+else if(q>p&&q>r)
 {
-printf("%d is greater\n",b);
+printf("%d is greater\n",q);
 }
 else
 {
-printf("%d is greater\n",c);
+printf("%d is greater\n",r);
 }
 return 0;
 }
