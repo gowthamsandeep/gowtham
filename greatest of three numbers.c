c39e35056e5a2553c81ce599dc76ed04e982b4c2
GowthamSandeep G <gowthamsandeep16@gmail.com>
2016-07-01 15:17:47 +0530

    Create greatest of three numbers.c

---
+++ greatest of three numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter the three numbers=");
+scanf("%d""%d""%d",&a,&b,&c);
+if(a>b&&a>c)
+{
+printf("%d is greater\n",a);
+}
+else if(b>a&&b>c)
+{
+printf("%d is greater\n",b);
+}
+else
+{
+printf("%d is greater\n",c);
+}
+return 0;
+}
