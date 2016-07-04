18e0402686fcdfab7816cc18e9c9b8059c5ede36
GowthamSandeep G <gowthamsandeep16@gmail.com>
2016-07-04 09:17:09 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a[100];
+int l;
+clrscr();
+printf("enter the words to count:=");
+scanf("%c",&a);
+l=strlen(a);
+printf("length is %d",l,a);
+getch();
+}
