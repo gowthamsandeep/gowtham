8067a4785a6eed4f23f215c45e3e963b1bda6501
GowthamSandeep G <gowthamsandeep16@gmail.com>
2016-07-06 11:24:34 +0530

    Create inclusive.c

---
+++ inclusive.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,sum2,sum;
+clrscr();
+for(a=0;a<15;a++)
+sum+=a;
+printf("1 to 15 %d sum=%d\n",sum);
+for(b=15;b<45;b++)
+if(b%2!=0)
+sum2+=b;
+printf("15 to 45  sum= %d\n",sum2);
+getch();
+}
