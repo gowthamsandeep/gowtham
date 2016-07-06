c679de015f327a860397e417ca379181ce648de1
GowthamSandeep G <gowthamsandeep16@gmail.com>
2016-07-06 12:15:29 +0530

    Create avg.c

---
+++ avg.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[100],b,i,avg,sum;
+scanf("%d",&b);
+scanf("%d",&a[i]);
+for(i=0;i<b;i++)
+sum+=a[i];
+avg=sum/b;
+printf("avg %d",avg);
+getch();
+}
+
