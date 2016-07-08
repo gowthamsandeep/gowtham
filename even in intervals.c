b3655b0e3b871eb46d0473e193a9fa9b689c5ae1
GowthamSandeep G <gowthamsandeep16@gmail.com>
2016-07-08 09:26:26 +0530

    Create even in intervals.c

---
+++ even in intervals.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,i;
+printf("enter the interval 1=",a);
+printf("\nenter the interval 2=",b);
+scanf("%d%d",&a,&b);
+for(i=a;i<=b;i++)
+{
+if(i%2!=1)
+printf("%d",i);
+}
+}
